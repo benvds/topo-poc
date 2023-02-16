fecc7421f52ea6354d8a6cfcae2e5f29209ab4e9
benvds <info@getlean.digital>
2023-02-16 15:07:10 +0100

    add crude pinch to zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,59 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { ReactComponent as Map } from "./assets/map.svg";
+import { useGesture } from "@use-gesture/react";
+
 import "./App.css";
 
 function App() {
-  const [zoom, setZoom] = useState(1);
+  const gestureTargetRef = useRef(null);
+  const [scale, setScale] = useState(1);
+  const [movementX, setMovementX] = useState(0);
+  const [movementY, setMovementY] = useState(0);
+  const [dragX, setDragX] = useState(0);
+  const [dragY, setDragY] = useState(0);
+  const [pinch, setPinch] = useState(1);
 
-  const zoomIn = useCallback(() => setZoom((prev) => prev + 1), [setZoom]);
-  const zoomOut = useCallback(() => setZoom((prev) => prev - 1), [setZoom]);
+  const zoomIn = useCallback(() => setScale((prev) => prev + 1), [setScale]);
+  const zoomOut = useCallback(() => setScale((prev) => prev - 1), [setScale]);
 
-  const translateX = 0;
-  const translateY = 0;
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useGesture(
+    {
+      onDrag: ({ movement: [mx, my] }) => {
+        setDragX(mx);
+        setDragY(my);
+        // setTranslateX((prev) => prev + mx);
+        // setTranslateY((prev) => prev + my);
+        // api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+      },
+      onDragEnd: ({ movement: [mx, my] }) => {
+        setDragX(0);
+        setDragY(0);
+        setMovementX((prev) => prev + mx);
+        setMovementY((prev) => prev + my);
+      },
+      onPinch: ({ offset }) => {
+        setPinch(offset[0] / scale);
+      },
+      onPinchEnd: ({ offset }) => {
+        setPinch(1);
+        setScale((prev) => {
+          console.debug("pinch end", prev * (offset[0] / prev));
+          return prev * (offset[0] / prev);
+        });
+      },
+    },
+    {
+      target: gestureTargetRef,
+    }
+  );
 
   const mapStyles = {
+    touchAction: "none",
     // transform: `scale(${zoom}, ${zoom})`,
     // transform: `matrix3d(${zoom}, 0, 0, 0, 0, ${zoom}, 0, 0, 0, 0, 1, 0, ${transformX}, ${transformY}, 0, 1)`, // blurry on safari
-    transform: `matrix(${zoom}, 0, 0, ${zoom}, ${translateX}, ${translateY})`,
+    transform: `matrix(${scale * pinch}, 0, 0, ${scale * pinch}, ${movementX + dragX
+      }, ${movementY + dragY})`,
   };
 
   return (
@@ -30,7 +69,7 @@ function App() {
       <div>
         <div id="svg-wrapper">
           {/* <img src={mapSvg} className="map-image" alt="map" style={mapStyles} /> */}
-          <div style={mapStyles}>
+          <div ref={gestureTargetRef} style={mapStyles}>
             <Map />
           </div>
         </div>
